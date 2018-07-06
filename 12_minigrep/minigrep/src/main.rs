48c649425a72dd74488d020162f3359d28c07ee8
Cory Stegelmeier <cstegel@gmail.com>
2018-07-05 20:23:04 -0400

    Add better error handling to file loading

--- 12_minigrep/minigrep/src/main.rs
+++ 12_minigrep/minigrep/src/main.rs
@@ -1,25 +1,51 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_configs(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("query: {}", query);
-    println!("file: {}", filename);
+    println!("query: {}", config.query);
+    println!("file: {}", config.filename);
 
-    let mut f = File::open(filename).expect("could not open file");
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut f = File::open(config.filename)?;
 
     let mut contents = String::new();
 
     f.read_to_string(&mut contents).expect("something went wrong reading the file");
     println!("contents: {}", contents);
+    Ok(())
 }
 
-fn parse_configs(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-    (query, filename)
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config{query, filename})
+    }
+
 }
